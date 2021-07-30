e56933ea4fac52b054b8c848ed93c03d48d977d6
GlenStautland <48930879+GlenStautland@users.noreply.github.com>
2021-07-30 21:01:57 +0200

    Db update
    Need to fix the json file for test data to db

--- EFDataAccessLibrary/Models/Address.cs
+++ EFDataAccessLibrary/Models/Address.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +11,18 @@ namespace EFDataAccessLibrary.Models
     public class Address
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(200)]
         public string StreetAdress { get; set; }
+        [Required]
+        [MaxLength(100)]
         public string City { get; set; }
+        [Required]
+        [MaxLength(50)]
         public string State { get; set; }
+        [Required]
+        [MaxLength(10)]
+        [Column(TypeName ="varchar(10)")]
         public string ZipCode { get; set; }
 
     }
